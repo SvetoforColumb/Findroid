5e9c287ffcd09d7b3f802ff3b5ba9c29afaa8586
foskarRU <s.polonski@mail.ru>
2019-03-12 01:04:15 +0500

    Login activity

--- app/src/main/java/ga/winterhills/findroid/LoginActivity.java
+++ app/src/main/java/ga/winterhills/findroid/LoginActivity.java
@@ -3,6 +3,7 @@ package ga.winterhills.findroid;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -277,6 +278,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
         mEmailView.setAdapter(adapter);
+    }
+
+    public void SignIn(View view) {
+        Intent intentObj = new Intent(this, MainActivity.class);
+        EditText editText = findViewById(R.id.password);
+        TextView textView = findViewById(R.id.email);
+        startActivity(intentObj);
     }
 
 
