576bb3d57ea2110ce9a3db08d192b24306e00ee5
Sasha Grigoryeff <a.grigoryeff@gmail.com>
2019-06-12 16:13:18 +0500

    Added drawing map blocks

--- app/src/main/java/ga/winterhills/findroid/DrawMap.java
+++ app/src/main/java/ga/winterhills/findroid/DrawMap.java
@@ -3,6 +3,7 @@ package ga.winterhills.findroid;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -25,28 +26,106 @@ import java.util.List;
 import ga.winterhills.findroid.R;
 
 public class DrawMap extends View {
-
+    boolean taskDone;
     private Paint mPaint = new Paint();
     private Rect mRect = new Rect();
     MapData mapData;
 
     public DrawMap(Context context) {
         super(context);
-
+        mapData = new MapData();
+        mapData.execute();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-
-
+        synchronized(this) {
+            try {
+                wait(1000);
+            } catch(InterruptedException ie){}
+        }
+        int width = getWidth();
+        int height = getHeight();
         // стиль Заливка
         mPaint.setStyle(Paint.Style.FILL);
         // закрашиваем холст белым цветом
         mPaint.setColor(Color.WHITE);
         canvas.drawPaint(mPaint);
         mPaint.setAntiAlias(true);
+        Bitmap map_block_0000 = BitmapFactory.decodeResource(getResources(), R.drawable.map_0000);
+        Bitmap map_block_0001 = BitmapFactory.decodeResource(getResources(), R.drawable.map_0001);
+        Bitmap map_block_0010 = BitmapFactory.decodeResource(getResources(), R.drawable.map_0010);
+        Bitmap map_block_0011 = BitmapFactory.decodeResource(getResources(), R.drawable.map_0011);
+        Bitmap map_block_0100 = BitmapFactory.decodeResource(getResources(), R.drawable.map_0100);
+        Bitmap map_block_0101 = BitmapFactory.decodeResource(getResources(), R.drawable.map_0101);
+        Bitmap map_block_0110 = BitmapFactory.decodeResource(getResources(), R.drawable.map_0110);
+        Bitmap map_block_0111 = BitmapFactory.decodeResource(getResources(), R.drawable.map_0111);
+        Bitmap map_block_1000 = BitmapFactory.decodeResource(getResources(), R.drawable.map_1000);
+        Bitmap map_block_1001 = BitmapFactory.decodeResource(getResources(), R.drawable.map_1001);
+        Bitmap map_block_1010 = BitmapFactory.decodeResource(getResources(), R.drawable.map_1010);
+        Bitmap map_block_1011 = BitmapFactory.decodeResource(getResources(), R.drawable.map_1011);
+        Bitmap map_block_1100 = BitmapFactory.decodeResource(getResources(), R.drawable.map_1100);
+        Bitmap map_block_1101 = BitmapFactory.decodeResource(getResources(), R.drawable.map_1101);
+        Bitmap map_block_1110 = BitmapFactory.decodeResource(getResources(), R.drawable.map_1110);
+        Bitmap map_block_1111 = BitmapFactory.decodeResource(getResources(), R.drawable.map_0001);
+        map_block_0000= map_block_0000.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_0001= map_block_0001.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_0010= map_block_0010.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_0011= map_block_0011.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_0100= map_block_0100.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_0101= map_block_0101.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_0110= map_block_0110.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_0111= map_block_0111.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_1000= map_block_1000.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_1001= map_block_1001.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_1010= map_block_1010.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_1011= map_block_1011.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_1100= map_block_1100.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_1101= map_block_1101.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_1110= map_block_1110.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_1111= map_block_1111.copy(Bitmap.Config.ARGB_8888, true);
+        map_block_0000.setHeight(height/10);
+        map_block_0000.setWidth(width/10);
+        map_block_0001.setHeight(height/10);
+        map_block_0001.setWidth(width/10);
+        map_block_0010.setHeight(height/10);
+        map_block_0010.setWidth(width/10);
+        map_block_0011.setHeight(height/10);
+        map_block_0011.setWidth(width/10);
+        map_block_0100.setHeight(height/10);
+        map_block_0100.setWidth(width/10);
+        map_block_0101.setHeight(height/10);
+        map_block_0101.setWidth(width/10);
+        map_block_0110.setHeight(height/10);
+        map_block_0110.setWidth(width/10);
+        map_block_0111.setHeight(height/10);
+        map_block_0111.setWidth(width/10);
+        map_block_1000.setHeight(height/10);
+        map_block_1000.setWidth(width/10);
+        map_block_1001.setHeight(height/10);
+        map_block_1001.setWidth(width/10);
+        map_block_1010.setHeight(height/10);
+        map_block_1010.setWidth(width/10);
+        map_block_1011.setHeight(height/10);
+        map_block_1011.setWidth(width/10);
+        map_block_1100.setHeight(height/10);
+        map_block_1100.setWidth(width/10);
+        map_block_1101.setHeight(height/10);
+        map_block_1101.setWidth(width/10);
+        map_block_1110.setHeight(height/10);
+        map_block_1110.setWidth(width/10);
+        map_block_1111.setHeight(height/10);
+        map_block_1111.setWidth(width/10);
+        final TypedArray styledAttributes = getContext().getTheme().obtainStyledAttributes(
+                new int[] { android.R.attr.actionBarSize });
+        int mActionBarSize = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+        canvas.drawBitmap(map_block_0000, 0, mActionBarSize, null);
+
+
+
+
         mPaint.setColor(Color.RED);
         mPaint.setStrokeWidth(5);
         canvas.drawLine(300,400,800,900, mPaint);
@@ -55,10 +134,8 @@ public class DrawMap extends View {
         canvas.drawCircle(800,900,50,mPaint);
         mPaint.setColor(Color.BLACK);
         mPaint.setTextSize(30);
-        canvas.drawText("Moscow", 400,400,mPaint);
+        canvas.drawText(String.valueOf(mapData.Map[0].block), 400,400,mPaint);
         canvas.drawText("Chelyabinsk", 900,900,mPaint);
-        int width = getWidth();
-        int height = getHeight();
         mPaint.setTextSize(40);
         //canvas.drawText(width + " " + height, 100, 300, mPaint);
         canvas.save();
@@ -67,6 +144,7 @@ public class DrawMap extends View {
 
 
     class MapData extends AsyncTask<Void, Void, Boolean> {
+        DrawMap drawMap;
         String[] nameOfCities={"Moscow", "Penza","Tomsk", "Sochi", "Azov", "Amursk", "Volgograd", "Kazan", "Ufa", "Chelyabinsk"};
         class Block{
             public int x;
@@ -80,9 +158,10 @@ public class DrawMap extends View {
             int right;
         }
 
-        Block Map[];
+        public Block Map[];
 
         MapData(){
+            //this.drawMap = drawMap;
             Map=new Block[100];
             for (int i=0; i<100; i++)
             {
@@ -142,3 +221,4 @@ public class DrawMap extends View {
     }
 
 }
+
